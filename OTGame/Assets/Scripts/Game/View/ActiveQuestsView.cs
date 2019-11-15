d885b96578e76a7d3e78db3269f32f3d755fe2df
anton <anton13395@gmail.com>
2019-11-15 13:55:23 +0300

    "Completed quests" section

--- OTGame/Assets/Scripts/Game/View/ActiveQuestsView.cs
+++ OTGame/Assets/Scripts/Game/View/ActiveQuestsView.cs
@@ -7,6 +7,8 @@ using UnityEngine.UI;
 
 public class ActiveQuestsView : MonoBehaviour
 {
+    List<Transform> questButtons;
+
     GameObject questList;
     GameObject questSelection;
     Text questDescription;
@@ -24,32 +26,52 @@ public class ActiveQuestsView : MonoBehaviour
         questSelection = transform.Find("background/Scroll View/Viewport/Selection").gameObject;
         questDescription = transform.Find("background/QuestDescription/Text").GetComponent<Text>();
 
-        for (int i = 1; i < questList.transform.childCount; i++)
-            Destroy(questList.transform.GetChild(1).gameObject);
+        for (int i = 2; i < questList.transform.childCount; i++)
+            Destroy(questList.transform.GetChild(2).gameObject);
 
         int questIndex = 0;
+        int uncompletedQuestIndex = 0;
         var listItemTemplate = questList.transform.Find("QuestNameTemplate");
-        listItemTemplate.gameObject.SetActive(false);
+
+        questButtons = new List<Transform>();
+
         foreach (var quest in quests)
         {
             var questButton = Instantiate(listItemTemplate, questList.transform, false);
-            questButton.gameObject.SetActive(true);
             questButton.name = "Quest [" + quest["title"].AsString + "]";
             questButton.GetComponent<Text>().text = quest["title"].AsString;
 
-            //questButton.transform.localPosition += Vector3.down * 21 * questIndex;
-
             questButton.GetComponent<Button>().onClick.AddListener(() => { ShowQuest(questIndex, quest); });
 
+            if (quest["isCompleted"].AsBool == false)
+                questButton.transform.SetSiblingIndex(uncompletedQuestIndex++);
+
+            questButtons.Add(questButton);
             questIndex++;
         }
 
-        ShowQuest(0, quests[0]);
+        listItemTemplate.gameObject.SetActive(false);
+
+        if (questIndex > 0)
+        {
+            LayoutRebuilder.ForceRebuildLayoutImmediate(questList.transform as RectTransform);
+            ShowQuest(0, quests[0]);
+        }
+        else ShowNoQuests();
+    }
+
+    void ShowNoQuests()
+    {
+        questDescription.text = "(no quests yet)";
+        questDescription.fontStyle = FontStyle.Italic;
+        questDescription.color = Color.gray;
+
+        questSelection.SetActive(false);
     }
 
     public void ShowQuest(int ind, JSONNode quest)
     {
-        var questButton = questList.transform.GetChild(ind + 1);
+        var questButton = questButtons[ind];
 
         var selLoc = questSelection.transform as RectTransform;
         selLoc.localPosition = new Vector3(selLoc.localPosition.x, questButton.localPosition.y, selLoc.localPosition.z);
