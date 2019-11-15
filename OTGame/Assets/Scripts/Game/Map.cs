cc639b91ccd0606bc819d68d9584851a6a41a2a8
anton <anton13395@gmail.com>
2019-11-15 12:21:18 +0300

    Player skin - Golem

--- OTGame/Assets/Scripts/Game/Map.cs
+++ OTGame/Assets/Scripts/Game/Map.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using NotSoSimpleJSON;
+using Spriter2UnityDX;
 using UnityEngine;
 
 public class Map : MonoBehaviour
 {
     public GameObject UnitPrefab;
+    public GameObject[] PlayerSkins;
 
     private PlayerController playerController;
     private GameObject mapSprite;
     private SpriteRenderer mapCursor;
 
     public Dictionary<string, Unit> Players { get; set; }
-    
+
     private MapModel MapModel => mapSprite.GetComponent<MapModel>();
     private IEnumerable<Monster> Monsters => Utils.ChildrenOf<Monster>(mapSprite.transform).Where(m => m.gameObject.activeSelf);
     private IEnumerable<Unit> QuestUnits => GetQuestUnits();
@@ -38,7 +40,7 @@ public class Map : MonoBehaviour
         Players = null;
     }
 
-    public void LoadLevel(JSONObject world) 
+    public void LoadLevel(JSONObject world)
     {
         UnloadLevel();
         if (playerController == null)
@@ -50,7 +52,7 @@ public class Map : MonoBehaviour
         mapSprite = Instantiate(Resources.Load<GameObject>("Maps/" + name), transform);
         mapSprite.SetActive(true);
 
-        foreach(var monster in map["monsters"].AsArray)
+        foreach (var monster in map["monsters"].AsArray)
         {
             var id = monster["id"].AsInt;
             if (monster["isAlive"].AsBool == false)
@@ -60,7 +62,7 @@ public class Map : MonoBehaviour
 
         Players = world["players"].AsArray.Select(u =>
         {
-            var unitObj = Instantiate(UnitPrefab, mapSprite.transform, false);
+            var unitObj = Instantiate(PlayerSkins.First(), mapSprite.transform, false);
 
             var unit = unitObj.AddComponent<Unit>();
             unit.Key = "p-" + u["name"].AsString;
@@ -72,23 +74,27 @@ public class Map : MonoBehaviour
         }).ToDictionary(u => u.Key);
 
         playerController.Player = Players["p-" + GameWorld.PlayerData.Name];
-        playerController.Player.GetComponent<SpriteRenderer>().sortingOrder = 7;
+        playerController.Player.GetComponent<EntityRenderer>().SortingOrder = 15;
     }
 
-    internal bool IsUnit(PointHex coord) 
+    internal bool IsUnit(PointHex coord)
         => QuestUnits.Any(q => q.CoordHex == coord);
 
     public void MonsterRespawned(int id)
     {
-        mapSprite.transform.Find("m-"+id)?.gameObject.SetActive(true);
+        mapSprite.transform.Find("m-" + id)?.gameObject.SetActive(true);
     }
 
 
-    public bool IsBlocked(PointHex pt, bool checkMonsters = false) => IsBlocked(pt.ToPoint(), checkMonsters);
+    public bool IsBlocked(PointHex pt, bool checkMonsters = false) 
+        => IsBlocked(pt.ToPoint(), checkMonsters);
 
     public bool IsBlocked(Vector2Int pt, bool checkMonsters = false)
     {
-        return pt.x < 0 || pt.y < 0 || pt.x >= MapModel.Width || pt.y >= MapModel.Height || MapModel.Passability[pt.y * MapModel.Width + pt.x] == 0 || checkMonsters && (Monsters.Any(m => m.Coord == pt) || Players.Values.Any(p => p.Coord == pt) || QuestUnits.Any(q => q.Coord == pt));
+        return pt.x < 0 || pt.y < 0 || pt.x >= MapModel.Width || pt.y >= MapModel.Height 
+            || MapModel.Passability[pt.y * MapModel.Width + pt.x] == 0 
+            || checkMonsters && Utils.ChildrenOf<Unit>(mapSprite.transform)
+                            .Any(u => u.gameObject.activeSelf && u.Coord == pt);
     }
 
 
@@ -99,7 +105,7 @@ public class Map : MonoBehaviour
 
     private IEnumerable<Unit> GetQuestUnits()
     {
-        foreach(var el in Utils.ChildrenOf<Unit>(mapSprite.transform))
+        foreach (var el in Utils.ChildrenOf<Unit>(mapSprite.transform))
             if (el.name.StartsWith("u-"))
                 yield return el;
     }
---
+++ OTGame/Assets/Scripts/Utils/UnitAnimator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class UnitAnimator : MonoBehaviour
+{
+    Unit unit;
+
+    void Start()
+    {
+        unit = GetComponent<Unit>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (unit.IsMoving)
+            transform.localScale = new Vector3(
+                ((unit.CoordHex.ToScreenPoint().x > unit.transform.position.x) ? 1 : -1) * Mathf.Abs(transform.localScale.x), 
+                transform.localScale.y, 
+                1
+            );
+        
+        unit.GetComponent<Animator>()?.SetBool("IsRunning", unit.IsMoving);
+    }
+}
